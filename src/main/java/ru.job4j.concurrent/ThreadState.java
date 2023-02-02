89582713789e56cb91c92e009207c62b81ee0689
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-02-02 17:19:14 +0300

    #229175 #387343

--- src/main/java/ru.job4j.concurrent/ThreadState.java
+++ src/main/java/ru.job4j.concurrent/ThreadState.java
@@ -2,22 +2,14 @@ package ru.job4j.concurrent;
 
 public class ThreadState {
     public static void main(String[] args) {
-        Thread first = new Thread(
-                () -> {
-                }
-        );
-        Thread second = new Thread(
-                () -> {
-                }
-        );
+        Thread first = new Thread( );
+        Thread second = new Thread( );
         System.out.println(first.getState());
         System.out.println(first.getState());
         first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
-        }
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
             System.out.println(second.getState());
         }
 
