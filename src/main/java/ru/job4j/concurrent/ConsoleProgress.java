d56fb85bef7cc57d1b3550c4fd50d52bd34e4d04
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-02-05 18:51:20 +0300

    #1019 #387687

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,7 +5,7 @@ public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(3000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        Thread.sleep(3000);
         progress.interrupt();
     }
 
@@ -13,22 +13,15 @@ public class ConsoleProgress implements Runnable {
     public void run() {
         var process = new String[]{"-", "\\", "|", "/"};
         int counter = 0;
-
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                Thread.sleep(100);
-                System.out.print("\r load: " + process[counter]);
-
-                if (counter + 1 == process.length) {
-                    counter = 0;
-                } else {
-                    counter = ++counter;
+                for (int i = 0; i < process.length; i++) {
+                    System.out.print("\r load: " + process[i]);
+                    Thread.sleep(100);
                 }
-
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-
         }
     }
 }
