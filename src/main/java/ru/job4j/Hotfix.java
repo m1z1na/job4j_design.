33d0517df3c2bcdccf4a0a2e1f9994083f1323f7
m1z1na <51057800+m1z1na@users.noreply.github.com>
2024-06-21 11:26:04 +0300

    3. @Builder [#504890]

--- src/main/java/ru/job4j/Hotfix.java
+++ src/main/java/ru/job4j/Hotfix.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 import ru.job4j.lombok.Category;
+import ru.job4j.lombok.Permission;
 
 public class Hotfix {
     public static void main(String[] args) {
@@ -9,5 +10,16 @@ public class Hotfix {
         System.out.println(category.getId());
         System.out.println(category.getName());
         System.out.println(category);
+
+
+        var permission = Permission.of()
+                .id(1)
+                .name("Lena")
+                .accessBy("play")
+                .accessBy("eat")
+                .accessBy("run")
+                .build();
+        System.out.println(permission);
+
     }
 }
